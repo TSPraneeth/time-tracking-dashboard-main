3529122fa1b5cebdd23a6782b41174f30837c4d9
TSPraneeth <tpraneeth98@gmail.com>
2021-11-15 01:37:39 +0530

    final

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-$(".clickable").click(function() {
-    console.log("hello");
-       $(".dropdown").toggle(); 
+
+$("a").click(function(e) {
+    console.log(e.target.className);
+    if(e.target.className == "daily_work_btn")
+    {
+        $(".dropdown_work").toggle();
+        $(".daily_work").css("display","block");
+        $(".weekly_work").css("display","none");
+        $(".monthly_work").css("display","none");
+    }
 });
 
-$(".second").click(function() {
+$(".weekly_all").click(function() {
     $(".daily").css("display","none");
     $(".weekly").css("display","block");
+    $(".monthly").css("display","none");
 });
 
-$.getJSON("data.json", function (data) {
-    console.log(data);
+$(".daily_all").click(function() {
+    $(".daily").css("display","block");
+    $(".weekly").css("display","none");
+    $(".monthly").css("display","none");
 });
+
+$(".monthly_all").click(function() {
+    $(".daily").css("display","none");
+    $(".weekly").css("display","none");
+    $(".monthly").css("display","block");
+});
+
+// if($('.fas').hover() == true) 
+// {
+//     $(".details").css("filter","brightness(1)");
+// }
+
+// else
+// {
+//     $(".details").css("filter","brightness(1.2)");
+// }
